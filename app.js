7ce230d2e0264e7aab05bea54f3b8da19be9d000
Konrad Szwedo <k.szwedo@partner.samsung.com>
2019-01-15 18:15:20 +0100

    [WIP] Temporary commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const puppeteer = require('puppeteer');
 const program = require('commander');
 const axios = require('axios');
 const moment = require('moment');
+const colors = require('colors');
 
 const platiniumBaseurl = 'https://platinium.perfectgym.pl/ClientPortal2/',
   actions = {
@@ -67,14 +68,14 @@ console.log(`Params: -u: ${program.username}, -p: ${program.password},-d: ${JSON
   await page.click(selectors.loginButton);
   //Logged
 
-  // await page.waitForSelector(selectors.bookAClassButton);
-  // await page.evaluate((selector) => {
-  //   document.querySelector(selector).click();
-  // }, [selectors.bookAClassButton]);
-  //
-  // await page.waitForSelector(selectors.searchInput);
-  // await page.focus(selectors.searchInput);
-  // await page.keyboard.type(activityName);
+  await page.waitForSelector(selectors.bookAClassButton);
+  await page.evaluate((selector) => {
+    document.querySelector(selector).click();
+  }, [selectors.bookAClassButton]);
+
+  await page.waitForSelector(selectors.searchInput);
+  await page.focus(selectors.searchInput);
+  await page.keyboard.type(activityName);
 
   let calendarData;
   await axios.post(platiniumBaseurl + actions.postWeeklyListClasses, {
@@ -106,43 +107,56 @@ console.log(`Params: -u: ${program.username}, -p: ${program.password},-d: ${JSON
   axios.defaults.headers['accept-language'] = 'en-US,en;q=0.9';
   axios.defaults.headers['cache-control'] = 'public, max-age=31536000';
 
-  await page.evaluate(function () {
-    fetch("https://platinium.perfectgym.pl/ClientPortal2/Classes/ClassCalendar/BookClass", {
-      "credentials": "include",
-      "headers": {
-        "accept": "application/json, text/plain, */*",
-        "accept-language": "en-US,en;q=0.9",
-        "cache-control": "public, max-age=31536000",
-        "content-type": "application/json;charset=UTF-8",
-        "cp-lang": "en",
-        "cp-mode": "desktop",
-        "x-requested-with": "XMLHttpRequest"
-      },
-      "referrer": "https://platinium.perfectgym.pl/ClientPortal2/",
-      "referrerPolicy": "no-referrer-when-downgrade",
-      "body": "{\"classId\":274629}",
-      "method": "POST",
-      "mode": "cors"
-    })
-      .then(result => console.log(result))
-      .catch(error => console.log(error));
-  });
+  // await page.evaluate(function () {
+  //   fetch("https://platinium.perfectgym.pl/ClientPortal2/Classes/ClassCalendar/BookClass", {
+  //     "credentials": "include",
+  //     "headers": {
+  //       "accept": "application/json, text/plain, */*",
+  //       "accept-language": "en-US,en;q=0.9",
+  //       "cache-control": "public, max-age=31536000",
+  //       "content-type": "application/json;charset=UTF-8",
+  //       "cp-lang": "en",
+  //       "cp-mode": "desktop",
+  //       "x-requested-with": "XMLHttpRequest"
+  //     },
+  //     "referrer": "https://platinium.perfectgym.pl/ClientPortal2/",
+  //     "referrerPolicy": "no-referrer-when-downgrade",
+  //     "body": "{\"classId\":274629}",
+  //     "method": "POST",
+  //     "mode": "cors"
+  //   })
+  //     .then(result => console.log(result))
+  //     .catch(error => console.log(error));
+  // });
 
-  await axios.get('https://platinium.perfectgym.pl/ClientPortal2/Classes/ClassCalendar/WeeklyListClasses', {clubId: "16"})
-    .then(() =>
-      console.log('Booked successfully!')
-    ).catch(error =>
-      console.log(`Error when booking [Name: ${booking.name}, Id: ${booking.id}, StartTime: ${booking.startTime}],
-       ErrorCode: ${JSON.stringify(error.response.status)}`)
-    );
+  // await axios.post('https://platinium.perfectgym.pl/ClientPortal2/Classes/ClassCalendar/WeeklyListClasses', {clubId: "16"})
+  //   .then((result) =>
+  //     console.log(`Booked successfully! [Status: ${result.status} ${result.statusText}]`)
+  //   ).catch(error =>
+  //     console.log(`Error when booking [Name: ${booking.name}, Id: ${booking.id}, StartTime: ${booking.startTime}],
+  //      ErrorCode: ${JSON.stringify(error.response.status)}`)
+  //   );
 
 
-  await axios.post(platiniumBaseurl + actions.bookClass, {
-    classId: '275492'
-  }).then(() =>
-    console.log('Booked successfully!')
-  ).catch(error =>
-    console.log(`Error when booking [Name: ${booking.name}, Id: ${booking.id}, StartTime: ${booking.startTime}], ErrorCode: ${JSON.stringify(error.response.status)}`)
+  await axios({
+    method: 'post',
+    url: platiniumBaseurl + actions.bookClass,
+    data: {classId: '272882'},
+    headers: {
+      cookie: "ClientPortal.Embed; .AspNet.ApplicationCookie=TKS0wCR89nSkFddTlCG3nKp4TRG8Ox6DqqSzDLSYXy2xQ9rPdJ3YMW5CAmDCv9X7pE2J8ze1q6S0H3FQl74m5NeikVcKW5fZMQ7kWPji7lCcbV3S55I8KJiX6ujpFuzeDdNOwwkLhsVyDPS7y1X7aXi5iD0KvV_U3MFH38-8-0AMrhJU26W-5d1xkF5fnvelLX6Gn0w8-AZUN" +
+        "mFhqML8TH2Mo0Hxl6k0mbrQnRjLeH_iYvmkBHjgioUZMBUdS3d9ojS5WjJ7_3u2MTCSGK8yaY1Fj-3tv7kKI8a5HRfUAWiuJR_LHQAZMuwxJ2wC43OC_k9AA0UeWr4Dee9yPe1Jglt4dTtl7ZouYXzn5_r178nP91U-Jpoi6cwn8RxLpuRcNlkDZDwr9iPfhxey767rzYrKjA9DbRCFnvwtd3GOhtChMaZlfM5MatD96iAMHP6xcFn2IXLv_3tjBEoYFtYn99DybwEQLfwyN0-uELt2iBXDsN8_Eo69PeO0VPMOTMYCjTJMYEh-GxEmnmQNM8M0g_uOBbegjM5SkjNikoBYReIoMjuVBmNrVWPFiXl1Sr97TZNFltP369yoZIgBPdCvn8cNsr2YhOs6Vl_6MJo8_VrnHf9P_TpE0StZEQ_-gAmR1vnTbL8fuPSwLw2BKiyeYClmGCWqWWhbJlPpFcdvCeCeas3clYoonuxbRYe2BcKD5NUNPKPrOLZReob7AsYhyoDVB8yzRo_MyuO8xPG_SMwxUlWN6ul1vnIogxfiluZQhDp4-U3qmpgqnq29oz0e_g",
+    }
+  }).then(result => {
+      if (result.status === 200) {
+        console.log('Booked successfully!'.green);
+      }
+    }
+  ).catch(result => {
+      if (result.response.status === 499 && result.response.data.Errors[0].Code === 'ClassAlreadyBooked') {
+        console.log('Already booked! OK'.green);
+      }
+      console.log(`Error when booking [Name: ${booking.name}, Id: ${booking.id}, StartTime: ${booking.startTime}], ErrorCode: ${JSON.stringify(result.response.status)}`.red)
+    }
   );
 
   await page.screenshot({path: 'platiniumPageScreenshot.png'});
